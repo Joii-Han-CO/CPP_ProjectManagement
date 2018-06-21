8a0b01aca3ef7ee356b01c220b438eb077190a6a
hanzishuang <hanzishuang@feinno.com>
2018-06-21 15:01:04 +0800

    加入预编译头能力

--- add.bat
+++ add.bat
@@ -1,7 +1,7 @@
 echo off
 set name=ipc
 set folder=lib
-set s_path=E:\code\fde_lib_new\fde_lib
+set s_path=E:\code\fde_lib_nex\fde_lib
 
 python .\python\add.py -p %s_path% -n %name% -f %folder%
 pause
--- create.bat
+++ create.bat
@@ -1,5 +1,5 @@
 echo off
-set sln_path=E:\code\fde_lib_new
+set sln_path=E:\code\fde_lib_nex
 set sln_name=fde_lib
 set sln_namespace=fde
 
